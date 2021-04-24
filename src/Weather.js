aa12ffebcba0058c8f0d48bb78622577f3d1fb70
deliahijon22 <79318393+deliahijon22@users.noreply.github.com>
2021-04-24 12:01:19 +0200

    weather forecast js and css added to project

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 import "./App.css";
@@ -11,6 +12,7 @@ export default function Weather(props) {
     console.log(response.data);
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
@@ -59,7 +61,7 @@ export default function Weather(props) {
             </form>
             <WeatherInfo data={weatherData}/>
           </nav>
-         
+         <WeatherForecast coordinates={weatherData.coordinates}/>
 
        
         </div>
